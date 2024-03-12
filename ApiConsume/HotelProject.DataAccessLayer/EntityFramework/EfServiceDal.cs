d5520c13698215604a94f39e5f8477d84d4bea97
ozangulll <ozan.gul1@ogr.sakarya.edu.tr>
2024-03-12 12:47:31 +0300

    for ef methods, EfDataAccessLayer added for each entity.

---
+++ ApiConsume/HotelProject.DataAccessLayer/EntityFramework/EfServiceDal.cs
@@ -0,0 +1,21 @@
+﻿using HotelProject.DataAccessLayer.Repositories;
+using HotelProject.DataAccessLayer.Abstract;
+using HotelProject.DataAccessLayer.Concrete;
+
+using HotelProject.EntityLayer.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HotelProject.DataAccessLayer.EntityFramework
+{
+   public class EfServiceDal:GenericRepository<Service>,IServiceDal
+    {
+        public EfServiceDal(Context context) : base(context)
+        {
+            
+        }
+    }
+}
--- ApiConsume/HotelProject.DataAccessLayer/Repositories/GenericRepository.cs
+++ ApiConsume/HotelProject.DataAccessLayer/Repositories/GenericRepository.cs
@@ -8,7 +8,7 @@ using System.Threading.Tasks;
 
 namespace HotelProject.DataAccessLayer.Repositories
 {
-    internal class GenericRepository<T> : IGenericDal<T> where T : class
+   public class GenericRepository<T> : IGenericDal<T> where T : class
     {
         private readonly Context _context;
 
