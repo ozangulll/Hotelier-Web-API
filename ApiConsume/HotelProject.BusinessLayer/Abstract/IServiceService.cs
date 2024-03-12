d26d3e4db1ad78c649d53844d9c05aac194d1e6d
ozangulll <ozan.gul1@ogr.sakarya.edu.tr>
2024-03-12 12:54:54 +0300

    for each entity generic service class created in businesslayer

--- ApiConsume/HotelProject.BusinessLayer/Abstract/IServiceService.cs
+++ ApiConsume/HotelProject.BusinessLayer/Abstract/IServiceService.cs
@@ -5,9 +5,9 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace HotelProject.DataAccessLayer.Abstract
+namespace HotelProject.BusinessLayer.Abstract
 {
-    public interface ISubscriberDal:IGenericDal<Subscribe>
+    public interface IServiceService:IGenericService<Service>
     {
     }
 }
